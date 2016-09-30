e249e2b3057b361c8f74696a3da70b12c656c8a5
Joshua Kartzman <emperorjoshua1@gmail.com>
2016-09-30 19:15:48 -0400

    This is the interface and the implementation for the Button Presser.

---
+++ TeamCode/src/main/java/ButtonPresserInterface.java
@@ -0,0 +1,11 @@
+/**
+ * Created by emper on 9/30/2016.
+ */
+public interface ButtonPresserInterface {
+    void Press_Button();
+    void Was_Button_Pressed();
+    boolean Is_Left_Side_Teamcolor();
+    boolean Is_Right_Side_Teamcolor();
+    void Get_Left_Color();
+    void Get_Right_Color();
+}
