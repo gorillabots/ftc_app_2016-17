aa90b0daae47cd0db232c56687f9707c05510c24
Jarred Moyer <jarredds941@gmail.com>
2016-10-07 21:40:45 -0400

    Today I removed Vuforia from the team files, this allowed us to sucessfully compile with ease. I also started wwork on a tank practice opmode that's sole purpose was to test the possibilities of tank drive on a holonomic drive train

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTest.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * Created by mikko on 9/30/16.
  */
 
-@TeleOp(name = "OmniwheelOp", group = "Concept")
+@TeleOp(name = "OmniwheelOpTank", group = "Concept")
 public class TankTest extends OpMode
 {
     DcMotor frontRight;
@@ -29,16 +29,28 @@ public class TankTest extends OpMode
     public void loop()
     {
 
+        float stickX = gamepad1.left_stick_x; // Stick position (Absolute heading)
+        float stickY = gamepad1.left_stick_y; // Each is in range -1 to 1
+
+        int facingDeg = 45; //Robot's rotation (possibly multiply by -1 to invert)
+        double facingRad = Math.toRadians(facingDeg); // Convert to radians
+
+        double cs = Math.cos(facingRad);
+        double sn = Math.sin(facingRad);
+
+        double headX = stickX * cs - stickY * sn; //Rotated vector (Relative heading)
+        double headY = stickX * sn + stickY * cs; //Each is in range -1 * root 2 to root 2
+
+        headX /= Math.sqrt(2); //In range -1 to 1
+        headY /= Math.sqrt(2);
+
+        if(gamepad1.right_stick_y >= .20 || gamepad1.right_stick_x >= .20 || gamepad1.right_stick_x <= -.20 || gamepad1.right_stick_y <= -.20){
 
 
-
-        if(gamepad1.right_stick_y >= 20 || gamepad1.right_stick_x >= 20 || gamepad1.right_stick_x <= -20 || gamepad1.right_stick_y <= -20){
-
-
-            frontLeft.setPower();
-            frontRight.setPower();
-            backLeft.setPower();
-            backRight.setPower();
+            frontLeft.setPower(headX);
+            frontRight.setPower(-headX);
+            backLeft.setPower(headY);
+            backRight.setPower(-headY);
 
 
 
@@ -76,6 +88,24 @@ public class TankTest extends OpMode
 */
 
         }
+        else{
+
+
+            if(gamepad1.right_stick_x >.30){
+                frontLeft.setPower(100);
+                frontRight.setPower(100);
+                backLeft.setPower(100);
+                backRight.setPower(100);
+            }
+            if(gamepad1.right_stick_x < -.30){
+                frontLeft.setPower(-100);
+                frontRight.setPower(-100);
+                backLeft.setPower(-100);
+                backRight.setPower(-100);
+            }
+
+        }
+
 
 
     }
