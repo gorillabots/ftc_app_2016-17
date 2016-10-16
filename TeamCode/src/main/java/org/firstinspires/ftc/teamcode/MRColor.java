f5fb7d0af993bbed76489b0034e2d1ee0e6665b8
Michelle <nishimototama@gmail.com>
2016-10-16 15:15:38 -0400

    led turns on

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MRColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MRColor.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by emper on 11/1/2015.
@@ -18,34 +18,26 @@ public class MRColor extends OpMode {
     String whatColorIs1Left;
     String whatColorIs2Left;
     String whatColorIsFloor;
+    ElapsedTime poemElapsed = new ElapsedTime();
+    boolean bPrevState = false;
+    boolean bLedOn = false;
 
     /**
-     * team color is red for this program
+     * team color is red
      */
 
     @Override
     public void init() {
 
-        Floorcolor.getI2cAddress().toString();
-        telemetry.addData("I2c", Floorcolor.getI2cAddress());
         Floorcolor = hardwareMap.colorSensor.get("Floorcolor_sensor");
         boolean bLedOn = true;
-        Floorcolor.enableLed(bLedOn);
-
-
+        // to count seconds of elapsed time
+        poemElapsed = new ElapsedTime();
     }
-
-    /**
-     * returns red or blue or none depending on what color is read from the floor sensor.
-     *
-     * @return String
-     */
-
 
     public String getFloorcolor() {
         String currentcolor = "none";
-
-
+        //find out color
         telemetry.addData("floorcolor-red", Floorcolor.red());
         telemetry.addData("floorcolor-blue", Floorcolor.blue());
         telemetry.addData("floorcolor-green", Floorcolor.green());
@@ -58,54 +50,37 @@ public class MRColor extends OpMode {
         if (Floorcolor.blue() < Floorcolor.red() && Floorcolor.red() < Floorcolor.green() && Floorcolor.green() < Floorcolor.alpha()) {
             currentcolor = "alpha";
         }
-
         return currentcolor;
     }
 
-    /**
-     * retruns red or blue or none depending on what color is read from the beacon sensor.
-     *
-     * @return String
-     */
-    //public String getBeaconcolor(ColorSensor color) {
-        //String currentcolor = "none";
-
-        //telemetry.addData("beconcolor-red", color.red());
-        //telemetry.addData("beconcolor-blue", color.blue());
-        //telemetry.addData("beconcolor-green", color.green());
-        //if (color.red() > color.blue() && color.red() > color.green() && color.green() == color.blue()) {
-            //currentcolor = "red";
-        //}
-        //telemetry.addData("beaconcolor-red", color.red());
-        //telemetry.addData("beconcolor-blue", color.blue());
-        //telemetry.addData("beconcolor-green", color.green());
-        //if (color.red() < color.blue() && color.green() < color.blue() && color.blue() > 1) {
-            //currentcolor = "blue";
-        //}
-
-        //return currentcolor;
-    //}
-boolean bPrevState = false;
-    boolean bLedOn = true;
     @Override
     public void loop() {
+
         telemetry.addData("floorcolor-red", Floorcolor.red());
         telemetry.addData("floorcolor-blue", Floorcolor.blue());
         telemetry.addData("floorcolor-green", Floorcolor.green());
-        whatColorIsFloor = getFloorcolor();
-        telemetry.addData("Floor_color", whatColorIsFloor);
+        telemetry.addData("time",poemElapsed.seconds());
+//        whatColorIsFloor = getFloorcolor();
+//        telemetry.addData("Floor_color", whatColorIsFloor);
         telemetry.update();
-        boolean X = gamepad1.x;
 
+        //turn on Led on sensor
+        if ((poemElapsed.seconds() >= 4.5) && (bLedOn == false)){
+            bLedOn = true;
+            Floorcolor.enableLed(bLedOn);
+
+        }
+        boolean X = gamepad1.x;
         // check for button-press state transitions.
         if ((X == true) && (X != bPrevState))  {
-
             // button is transitioning to a pressed state. Toggle the LED.
             bLedOn = !bLedOn;
             Floorcolor.enableLed(bLedOn);
-            }
+        }
 
         // update previous state variable.
         bPrevState = X;
+
+
     }
 }
