ca62e393f4b1a4d6b3d0401279fbeb19312d5754
Michelle <nishimototama@gmail.com>
2016-10-18 19:14:16 -0400

    changed RGB for null

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorS.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
@@ -7,23 +9,20 @@ import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 /**
  * Created by Owner on 10/7/2016.
  */
-
-public class ColorS extends OpMode{
+@Autonomous(name = "Sensor: sensorRGB", group = "Sensor")
+public class ColorS extends LinearOpMode{
 
     ColorSensor sensorRGB;
     DeviceInterfaceModule cdim;
     static final int LED_CHANNEL = 5;
 
-    @Override
-    public void init() {
-        boolean bPrevState = false;
-        boolean bCurrState = false;
-
+        public void runOpMode() {
         float hsvValues[] = {0F,0F,0F};
-        // F is 15
-        boolean bLedOn = true;
-
-        // turn the LED on in the beginning, just so user will know that the sensor is active.
+            boolean bCurrState = false;
+            boolean bPrevState = false;
+            boolean bLedOn = true;
+        
+            // turn the LED on in the beginning, just so user will know that the sensor is active.
         cdim.setDigitalChannelState(LED_CHANNEL, bLedOn);
 
         final float values[] = hsvValues;
@@ -38,25 +37,12 @@ public class ColorS extends OpMode{
 
         sensorRGB = hardwareMap.colorSensor.get("color");
 
-
         telemetry.addData("LED", bLedOn ? "On" : "Off");
         telemetry.addData("Clear", sensorRGB.alpha());
         telemetry.addData("Red  ", sensorRGB.red());
         telemetry.addData("Green", sensorRGB.green());
         telemetry.addData("Blue ", sensorRGB.blue());
         telemetry.addData("Hue", hsvValues[0]);
-
         telemetry.update();
-
     }
-
-    @Override
-    public void loop() {
-
-
-
-
-    }
-
-
 }
