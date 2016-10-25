f309cd26497e4efabb4d3dd01a25f854aee07851
Jarred Moyer <jarredds941@gmail.com>
2016-10-25 18:53:00 -0400

    created compTele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompTele.java
@@ -1,8 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 /**
  * Created by Jarred on 10/18/2016.
  */
 
-public class CompTele {
+public class CompTele extends OpMode {
+    Drivetrain drivetrain;
+
+    ModernRoboticsI2cGyro gyro;
+
+    int rotation = 0;
+
+    public void init()
+    {
+        drivetrain = new Drivetrain(hardwareMap, telemetry);
+
+        gyro = (ModernRoboticsI2cGyro)hardwareMap.gyroSensor.get("gyro");
+
+        gyro.calibrate();
+
+        try
+        {
+            // make sure the gyro is calibrated.
+            while (gyro.isCalibrating())
+            {
+                Thread.sleep(50);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        gyro.resetZAxisIntegrator();
+
+    }
+
+    public void loop()
+    {
+        float stickX = gamepad1.left_stick_x; // Stick position (Absolute heading)
+        float stickY = gamepad1.left_stick_y; // Each is in range -1 to 1
+
+        float stickRot = gamepad1.right_stick_x / 2f; //Used to rotate the robot;
+
+        rotation = gyro.getHeading();
+
+        drivetrain.oneStickLoop(stickX, stickY, stickRot, rotation);
+
+
+    }
 }
