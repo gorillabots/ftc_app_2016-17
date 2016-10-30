ae1aac2ceb163a94cceb3d3d4c8b4af2c99b1121
Jarred Moyer <jarredds941@gmail.com>
2016-10-30 18:45:52 -0400

    created the demo program as well as a test program for forklift

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/forkTEst.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Jarred on 10/30/2016.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "forkTest", group = "Concept")
+public class forkTEst extends OpMode {
+
+    DcMotor motor;
+
+
+
+        public void init()
+        {
+
+        motor = hardwareMap.dcMotor.get("motor");
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        }
+
+        public void loop()
+        {
+            //change the checksum ASAP
+            if(gamepad1.left_stick_y >.5){
+                motor.setPower(5);
+            }
+            else if(gamepad1.left_stick_y < -.5){
+                motor.setPower(-5);
+            }
+            else{
+            motor.setPower(0);
+        }
+
+        }
+
+    }
+
