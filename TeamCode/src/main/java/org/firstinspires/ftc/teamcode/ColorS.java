f871b5755b43419fc0a3474c6d1346d2290d3e8e
Michelle <nishimototama@gmail.com>
2016-11-22 19:41:58 -0500

    added variable for sat & tseting values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorS.java
@@ -18,34 +18,48 @@ import com.qualcomm.robotcore.hardware.DigitalChannelController;
 public class ColorS extends OpMode {
 
     ColorSensor sensorRGB;
+    DeviceInterfaceModule cdim;
     static final int LED_CHANNEL = 5;
     int count;
+    boolean bLedOn = false;
+int recounter = 0;
+
     public void init() {
-
         count = 0;
-
-        // turn the LED on in the beginning, just so user will know that the sensor is active.
+        cdim = hardwareMap.deviceInterfaceModule.get("dim");
+        cdim.setDigitalChannelMode(LED_CHANNEL, DigitalChannelController.Mode.OUTPUT);
         sensorRGB = hardwareMap.colorSensor.get("sensorRGB");
+        cdim.setDigitalChannelState(LED_CHANNEL, bLedOn);
     }
-        public void loop(){
-            float hsvValues[] = {0F, 0F, 0F};
 
-            final float values[] = hsvValues;
-            boolean bCurrState = false;
-            boolean bPrevState = false;
-            boolean bLedOn = true;
-            Color.RGBToHSV((sensorRGB.red() * 255) / 800, (sensorRGB.green() * 255) / 800, (sensorRGB.blue() * 255) / 800, hsvValues);
+    public void loop() {
+        float hsvValues[] = {0F, 0F, 0F};
 
-            //bPrevState = bCurrState;
-                telemetry.addData("LED", bLedOn ? "On" : "Off");
-                telemetry.addData("Clear", sensorRGB.alpha());
-                telemetry.addData("Red  ", sensorRGB.red());
-                telemetry.addData("Green", sensorRGB.green());
-                telemetry.addData("Blue ", sensorRGB.blue());
-                telemetry.addData("Hue", hsvValues[0]);
-                telemetry.addData("count is ",count);
+        // final float values[] = hsvValues;
+        boolean bCurrState = false;
+        boolean bPrevState = false;
 
-                telemetry.update();
-            }
-        }
+        Color.RGBToHSV((sensorRGB.red() * 255) / 800, (sensorRGB.green() * 255) / 800, (sensorRGB.blue() * 255) / 800, hsvValues);
+
+        //bPrevState = bCurrState;
+        telemetry.addData("LED", bLedOn ? "On" : "Off");
+        telemetry.addData("Clear", sensorRGB.alpha());
+        telemetry.addData("Red  ", sensorRGB.red());
+        telemetry.addData("Green", sensorRGB.green());
+        telemetry.addData("Blue ", sensorRGB.blue());
+        telemetry.addData("Hue", hsvValues[0]);
+        telemetry.addData("Saturation", hsvValues[1]);
+        telemetry.addData("count is ", count);
+        //telemetry.addData("number of reads", recounter);
+
+        telemetry.update();
+
+        recounter ++;
+    }
+    @Override
+    public void stop(){
+        telemetry.addData("number of reads", recounter);
+        telemetry.update();
+    }
+}
 
