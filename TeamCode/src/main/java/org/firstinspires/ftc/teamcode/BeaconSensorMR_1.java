0ce27068df43fbe552820e3ef23391a3d05df820
Michelle <nishimototama@gmail.com>
2017-01-03 19:11:19 -0500

    New color sensor for right side. Config is RightBS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconSensorMR_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconSensorMR_1.java
@@ -1,33 +1,3 @@
-/* Copyright (c) 2015 Qualcomm Technologies Inc
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Qualcomm Technologies Inc nor the names of its contributors
-may be used to endorse or promote products derived from this software without
-specific prior written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 package org.firstinspires.ftc.teamcode;
 import android.app.Activity;
 import android.graphics.Color;
@@ -37,11 +7,12 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @Disabled
 @TeleOp(name = "Sensor: MR Color", group = "Sensor")
 
-public class SensorMRColor extends LinearOpMode {
+public class BeaconSensorMR_1 extends LinearOpMode {
 
     ColorSensor colorSensor;    // Hardware Device Object
     float hsvValues[] = {0F,0F,0F};
@@ -49,16 +20,15 @@ public class SensorMRColor extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        // values is a reference to the hsvValues array.
 
         ElapsedTime opmodeRunTime = new ElapsedTime();
-        colorSensor = hardwareMap.colorSensor.get("BeaconSensor");
+        colorSensor = hardwareMap.colorSensor.get("RightBS");
+        colorSensor.setI2cAddress(I2cAddr.create8bit(59));
 
         waitForStart();
         while (opModeIsActive()) {
 
-
-            String currentcolor = getBeaconColor();
+            String currentcolor = getRightBeaconColor();
 
             // send the info back to driver station using telemetry function.
             telemetry.addData("Running Time", opmodeRunTime.seconds());
@@ -74,7 +44,7 @@ public class SensorMRColor extends LinearOpMode {
         }
     }
 
-    public String getBeaconColor() {
+    public String getRightBeaconColor() {
         Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
 
         String currentcolor = "blank";
