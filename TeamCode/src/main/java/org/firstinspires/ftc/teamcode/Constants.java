b5b756ac947d288a2a5a3f466f4055d6c44ef342
Mikko Neukirch <neukirchm@stny.rr.com>
2017-01-15 20:33:30 +0100

    Removed actuator stuff, removed unnecessary constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -8,12 +8,8 @@ public class Constants {
     public final static double WALL_TO_WALL_IN_AUTONOMOUS = 1.42;
     public final static double GO_BACKWARD_AFTER_TOUCH_WALL = 0.1;
 
-    public final static double ACTUATOR_DISTANCE_TO_HIT_BEACON = 0.35;
-    public final static double ACTUATOR_RESET_VALUE = 0.15;
-    public final static long ACTUATOR_TIME_OF_EXTENDING = 3000;
     public final static double BETWEEN_WHITE_LINES = 0.9;
     public final static double GO_BACK_AFTER_PRESSING_FIRST = 0.02;
-    public final static long ACTUATOR_RESET_TIME = 1000;
 
     public final static int DIAGONAL_INCREMENTS = 3718; //5240
     public final static int STRAIGHT_INCREMENTS = 2740; //3890
