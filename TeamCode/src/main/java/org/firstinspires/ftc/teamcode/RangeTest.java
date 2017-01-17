22b4ecb2c45643e3807df720da1f4c8fdb216204
Mikko Neukirch <neukirchm@stny.rr.com>
2017-01-17 01:25:33 +0100

    Converted RangeTest to TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RangeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RangeTest.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -10,19 +11,39 @@ import com.qualcomm.robotcore.hardware.Servo;
  */
 
 @Autonomous(name="RangeTest", group="test")
-public class RangeTest extends LinearOpMode{
-    AutonomousDriveTrain drive;
+public class RangeTest extends OpMode{
+    Drivetrain drive;
     UltrasonicSensor range;
-    public void runOpMode() {
-        range = hardwareMap.ultrasonicSensor.get("range");
-        drive = new AutonomousDriveTrain();
-        drive.init(this);
-        while(range.getUltrasonicLevel() > 25){
-            drive.right_continuous(0.5);
+    ModernRoboticsI2cGyro gyro;
+    public void init() {
+        drive = new Drivetrain(hardwareMap, telemetry);
+        gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+        gyro.calibrate();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        if(range.getUltrasonicLevel() < 25){
-            drive.right_continuous(0);
+        if (gyro.isCalibrating()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+    }
+    public void loop() {
+
+            float stickX = (gamepad1.left_stick_x); // Stick position (Absolute heading)
+            float stickY = (gamepad1.left_stick_y); // Each is in range -1 to 1
+            float stickRot = (gamepad1.right_stick_x / 2f); //Used to rotate the robot;
+            if(gamepad1.b == true) {
+                gyro.resetZAxisIntegrator();
+            }
+
+            int rotation = gyro.getHeading();
+
+            drive.oneStickLoop(stickX, stickY, stickRot, rotation, gamepad1.back);
     }
 
     }
