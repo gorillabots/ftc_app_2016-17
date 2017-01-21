4154847c4efa442cf802b9ebb20c2f74cfda6910
Michelle <nishimototama@gmail.com>
2017-01-20 20:00:41 -0500

    un disabled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Floorcolor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Floorcolor.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.util.ElapsedTime;
-@Disabled
+
 @TeleOp(name = "Sensor: MR Color", group = "Sensor")
 
 public class Floorcolor extends LinearOpMode {
