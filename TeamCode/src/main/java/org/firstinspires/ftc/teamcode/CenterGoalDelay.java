d476e379353def4f13170e8060a759ac298402ff
Mikko Neukirch <neukirchm@stny.rr.com>
2017-01-21 02:00:13 +0100

    Commented and removed imports from lots of files, formatted DriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterGoalDelay.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterGoalDelay.java
@@ -2,18 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Hardware;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.I2cAddr;
-import com.qualcomm.robotcore.hardware.Servo;
-import static org.firstinspires.ftc.teamcode.Constants.*;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 /**
  * Created by Jarred on 12/10/2016.
