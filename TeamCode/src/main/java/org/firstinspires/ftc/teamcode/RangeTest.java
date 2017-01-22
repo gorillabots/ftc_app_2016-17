05bc4bfd92c36a498ae3a6c2497d5db9310d95fc
Jarred Moyer <jarredds941@gmail.com>
2017-01-22 15:31:40 -0500

    converted rangeTest to a drivable opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RangeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RangeTest.java
@@ -1,28 +1,92 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.hardware.UltrasonicSensor;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.I2cAddr;
-import com.qualcomm.robotcore.hardware.Servo;
+
 /**
- * Created by emper on 1/13/2017.
+ * Created by Jarred on 10/30/2016.
  */
 
-@Autonomous(name="RangeTest", group="test")
-public class RangeTest extends LinearOpMode{
-    AutonomousDriveTrain drive;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "range test", group = "Concept")
+public class RangeTest extends OpMode {
+
+    Drivetrain drivetrain;
+    ModernRoboticsI2cGyro gyro;
+
+    DcMotor elevator;
+    DcMotor vac;
+    DcMotor fly;
+    DcMotor raise;
+    TouchSensor limit;
+
+    ColorSensor floorColor;
+    ColorSensor beaconColor;
+    Servo servoSwing;
+    long startTime;
+    long time;
+
     UltrasonicSensor range;
-    public void runOpMode() throws InterruptedException{
+    public void init() {
+
+        drivetrain = new Drivetrain(hardwareMap, telemetry);
+        gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+        gyro.calibrate();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (gyro.isCalibrating()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        gyro.resetZAxisIntegrator();
         range = hardwareMap.ultrasonicSensor.get("range");
-        drive = new AutonomousDriveTrain();
-        drive.init(this);
-        while(range.getUltrasonicLevel() > 25){
-            drive.right_continuous(0.5);
-        }
-        if(range.getUltrasonicLevel() < 25){
-          //  drive.stop(5000);
-        }
-    }
 
     }
+
+
+
+    public void loop() {
+        servoSwing.setPosition(.56);
+        float stickX = (gamepad1.left_stick_x); // Stick position (Absolute heading)
+        float stickY = (gamepad1.left_stick_y); // Each is in range -1 to 1
+        float stickRot = (gamepad1.right_stick_x / 2f); //Used to rotate the robot;
+
+
+
+
+        if (gamepad1.b == true) {
+            gyro.resetZAxisIntegrator();
+        }
+
+        int rotation = gyro.getHeading();
+
+        drivetrain.oneStickLoop(stickX, stickY, stickRot, rotation, gamepad1.back);
+        telemetry.addData("level is ", range.getUltrasonicLevel());
+
+    }
+
+    @Override
+    public void stop()
+    {
+        super.stop();
+        floorColor.enableLed(false);
+    }
+
+
+}
+
