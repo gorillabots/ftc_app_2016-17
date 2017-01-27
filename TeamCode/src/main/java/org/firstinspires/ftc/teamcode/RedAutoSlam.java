01941ca3e8a804b51d840095fc312b58b0a81bdc
Jarred Moyer <jarredds941@gmail.com>
2017-01-27 18:25:55 -0500

    reverted redAuto slam to 90% version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoSlam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoSlam.java
@@ -64,11 +64,6 @@ public class RedAutoSlam extends LinearOpMode
         beaconColorR.enableLed(false);
         driveTrain.beaconResponse(TeamColors.RED, beaconColorL, beaconColorR);
 
-        driveTrain.left(.1, .5);
-        driveTrain.back(.25, .8);
-        floorColor.enableLed(true);
-        driveTrain.backGyroToLine(floorColor, .22, 1, .05);
-        floorColor.enableLed(false);
 
         //Finishing up
 
