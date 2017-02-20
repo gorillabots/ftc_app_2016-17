7f2a4d38730ad930cf347c00dde4fa6251a8419a
Mikko Neukirch <neukirchm@stny.rr.com>
2017-02-19 20:07:15 -0500

    Realized we probably need the demo bot opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DemoBot.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.submodules.Drivetrain;
+
+/**
+ * Created by Jarred on 10/30/2016.
+ */
+
+@TeleOp(name = "DemoBot", group = "Final")
+public class DemoBot extends OpMode {
+
+
+    ElapsedTime timer;
+
+    Drivetrain drivetrain;
+
+
+    Servo claw;
+
+    DcMotor rotateArm;
+    ModernRoboticsI2cGyro gyro;
+
+    int rotation = 0;
+    int clawState = 0;
+
+
+
+
+    float clawOpen =1 ;
+    float clawClose = 0;
+    void toggleClaw(){
+
+        if(clawState== 1){
+            claw.setPosition(clawOpen);
+            clawState =2;
+            timer.reset();
+        }
+
+       else if(clawState == 2){
+                claw.setPosition(clawClose);
+                clawState = 1;
+                timer.reset();
+        }
+    }
+    public void init()
+    {
+
+
+        timer = new ElapsedTime();
+        drivetrain = new Drivetrain(hardwareMap, telemetry);
+
+        gyro = (ModernRoboticsI2cGyro)hardwareMap.gyroSensor.get("gyro");
+
+        rotateArm = hardwareMap.dcMotor.get("rotateArm");
+        claw = hardwareMap.servo.get("claw");
+
+        gyro.calibrate();
+
+        try
+        {
+            // make sure the gyro is calibrated.
+            while (gyro.isCalibrating())
+            {
+                Thread.sleep(50);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        gyro.resetZAxisIntegrator();
+
+        timer.startTime();
+    }
+
+    public void loop()
+    {
+        float stickX = gamepad1.left_stick_x; // Stick position (Absolute heading)
+        float stickY = gamepad1.left_stick_y; // Each is in range -1 to 1
+
+        float stickRot = gamepad1.right_stick_x / 2f; //Used to rotate the robot;
+
+        if(gamepad1.a)
+        {
+            gyro.resetZAxisIntegrator();
+        }
+
+        rotation = gyro.getHeading();
+
+        drivetrain.oneStickLoop(stickX, stickY, stickRot, rotation,gamepad1.back);
+
+        if(gamepad1.left_bumper == true){
+            rotateArm.setPower(.2);
+        }
+        else if(gamepad1.left_trigger > .5){
+            rotateArm.setPower(-.2);
+        }
+        else{
+            rotateArm.setPower(0);
+        }
+
+
+        if(gamepad1.right_bumper == true){
+            claw.setPosition(1.0);
+
+        }
+
+        else if(gamepad1.right_trigger > .5){
+            claw.setPosition(.2);
+
+        }
+
+
+
+    }
+
+}
