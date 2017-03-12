deb292f78fd59d2dc0d01d157529a3358f8de007
Jarred Moyer <jarredds941@gmail.com>
2017-03-12 12:47:20 -0400

    Led test updated to pulse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LedTestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LedTestOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.LedStates;
 import org.firstinspires.ftc.teamcode.submodules.LedController;
@@ -16,8 +17,26 @@ public class LedTestOp extends LinearOpMode {
     public void runOpMode() {
         led = new LedController(hardwareMap, telemetry);
         led.setLedState(LedStates.ON);
+        ElapsedTime timer = new ElapsedTime();
         //led.LedFlash(30, 0.5);
         waitForStart();
+        timer.startTime();
+        boolean ledOn = false;
+        while(timer.milliseconds()<10000){
+
+            if(timer.milliseconds()%500 == 0){
+                if(ledOn){
+                    led.setLedState(LedStates.ON);
+                    ledOn = true;
+                }
+                else{
+                    led.setLedState(LedStates.OFF);
+                    ledOn = false;
+                }
+            }
+
+        }
+
     }
 }
 
