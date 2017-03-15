b22e9a3626e7cd5afb31e3cd65f94cc5d42bc3bd
Joshua Kartzman <emperorjoshua1@gmail.com>
2017-03-15 19:46:45 -0400

    Changes to LED

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/submodules/LedController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/submodules/LedController.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.submodules;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -20,11 +21,13 @@ public class LedController implements LedControlInterface {
     private static final double LED_ON = 1.0;
     private static final double LED_OFF = 0.0;
     private LedStates currentState;
-    private double flash_start;
+
     public LedController(HardwareMap map, Telemetry tele) {
         controlPort = map.dcMotor.get("ledcontrol");
         setLedState(LedStates.OFF, null, null);
+        this.telemetry = tele;
     }
+
     @Override
     public void setLedState(LedStates state, Double time, Double interval) {
         currentState = state;
@@ -39,46 +42,64 @@ public class LedController implements LedControlInterface {
                 break;
             case FLASHING:
                 flashingThreadHandle = new Thread(new Flashing(time, interval));
-                if(flashingThreadHandle.isAlive()){}
-                if(!flashingThreadHandle.isAlive()){
+                if (flashingThreadHandle.isAlive()) {
+                }
+                if (!flashingThreadHandle.isAlive()) {
                     flashingThreadHandle.start();
                 }
                 break;
         }
     }
+
     @Override
     public LedStates getState() {
         return currentState;
     }
-    private class Flashing implements Runnable{
+
+    private class Flashing implements Runnable {
         private volatile boolean close = false;
         private double time;
         private long interval;
-        public Flashing(Double time, Double interval){
-            this.time = time*1000;
-            this.interval = (long)(interval*1000);
+
+        public Flashing(Double time, Double interval) {
+            this.time = time * 1000;
+            this.interval = (long) (interval * 1000);
         }
-        public void LedFlash(long interval) {
-            if (currentState != LedStates.FLASHING) {currentState = LedStates.FLASHING;}
+
+        public void LedFlash(long interval) throws InterruptedException {
+            if (currentState != LedStates.FLASHING) {
+                currentState = LedStates.FLASHING;
+            }
             if (getState() == LedStates.ON) {
                 controlPort.setPower(LED_OFF);
-                try {Thread.sleep(interval);} catch(InterruptedException e){}
+                Thread.sleep(interval);
             } else if (getState() == LedStates.OFF) {
                 controlPort.setPower(LED_ON);
-                try {Thread.sleep(interval);} catch(InterruptedException e){}
+                Thread.sleep(interval);
             }
         }
+
         @Override
         public void run() {
             ElapsedTime timer = new ElapsedTime();
             timer.reset();
-            while(!close && (timer.milliseconds() < this.time)){LedFlash(this.interval);}
+            while (!close && (timer.milliseconds() < this.time)) {
+
+                try {
+                    LedFlash(this.interval);
+                } catch (InterruptedException e) {
+                    this.close();
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
-        public void close(){
-            if(flashingThreadHandle.isAlive()) {
+
+        public void close() {
+            if (flashingThreadHandle.isAlive()) {
                 close = true;
                 currentState = LedStates.OFF;
                 controlPort.setPower(LED_OFF);
+                Thread.currentThread().interrupt();
             }
         }
     }
