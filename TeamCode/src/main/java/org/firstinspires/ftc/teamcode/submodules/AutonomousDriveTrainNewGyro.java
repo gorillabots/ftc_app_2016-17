d2f3c9c2ddbf64fc924c6712bf42e437ee67e510
Mikko <neukirchm@stny.rr.com>
2017-04-14 18:33:54 -0400

    Updated offset automatically while turning in autonomous using AutonomousDriveTrainNewGyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/submodules/AutonomousDriveTrainNewGyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/submodules/AutonomousDriveTrainNewGyro.java
@@ -78,12 +78,6 @@ public class AutonomousDriveTrainNewGyro
         offsetConverted = convertHeading(offset);
     }
 
-    public void updateOffset(double offset)
-    {
-        this.offset = offset;
-        offsetConverted = convertHeading(offset);
-    }
-
     public void stop()
     {
         navx.close();
@@ -691,6 +685,9 @@ public class AutonomousDriveTrainNewGyro
 
     public void turn(double target, double accuracy, double speed)
     {
+        offset += target;
+        offsetConverted = convertHeading(offset);
+
         navXPIDController pidController = new navXPIDController(navx, navXPIDController.navXTimestampedDataSource.YAW);
 
         pidController.setSetpoint(convertHeading(offset + target));
